926b3b077089354cf5315950127f284e01a4f13e
KangwookYoon <61686336+theia25@users.noreply.github.com>
2026-02-04 02:24:45 +0900

    Update ZippyCharacter.cpp

--- Client-Side Prediction/Setup & Sprint/ZippyCharacter.cpp
+++ Client-Side Prediction/Setup & Sprint/ZippyCharacter.cpp
@@ -1,4 +1,5 @@
-###생성자 변경
+#include "ZippyCharacter.h"
+
 // ACharacter 생성자를 호출하기 전에 FObjectInitializer를 통해 설정 조작
 // - 엔진이 제공하는 CMC 대신 UZippyCharacterMovementComponent로 클래스 생성 요청
 AZippyCharacter::AZippyCharacter(const FObjectInitializer& ObjectInitializer)
