cbff8847eb0f6728c34aa443161238e2b19b6dae
KangwookYoon <61686336+theia25@users.noreply.github.com>
2026-02-09 13:07:48 +0900

    Update ZippyCharacter.cpp
    Collision Params Setting

---
+++ Client-Side Prediction/Crouch & PlayerCameraManager/ZippyCharacter.cpp
@@ -0,0 +1,13 @@
+#include "ZippyCharacterMovementComponent.h"
+
+FCollisionQueryParams AZippyCharacter::GetIgnoreCharacterParams() const
+{
+	FCollisionQueryParams Params;
+
+	TArray<AActor*> CharacterChildren;
+	GetAllChildActors(CharacterChildren);
+	Params.AddIgnoredActors(CharacterChildren);
+	Params.AddIgnoredActor(this);
+
+	return Params;
+}
